adb22fd19eb7cca67a669c1b9f5fba9fc91cc1f6
jamshidsalayev1998 <jmlaravel@mail.ru>
2022-11-21 17:36:22 +0500

    asd

--- src/pages/examinationUser/FinalAccessAdminCrud/Pages/ExamProcessStudentTabs/ExamProcessByComputerTab.jsx
+++ src/pages/examinationUser/FinalAccessAdminCrud/Pages/ExamProcessStudentTabs/ExamProcessByComputerTab.jsx
@@ -1,19 +1,63 @@
 import React, {useContext, useEffect, useState} from "react";
 import {withTranslation} from "react-i18next";
-import {Table, Row, Col, Card, Tooltip, Skeleton, message, Popconfirm} from "antd";
+import {Table, Row, Col, Card, Tooltip, Skeleton, message, Popconfirm, notification} from "antd";
 import {CardBody, Container,} from "reactstrap";
 import {computerTestEndApi, getExamProcessComputers} from "../../../../../services/api_services/final_test_admin_api";
 import {clearComputerApi} from "../../../../../services/api_services/computers_api";
+import {socketParam} from "../../../../../App";
+import MainContext from "../../../../../Context/MainContext";
 
 const ExamProcessByComputerTab = (props) => {
     const {reload, setReload} = props;
     const [data, setData] = useState([]);
+    const mainContext = useContext(MainContext);
+    // console.log('uy' , context);
+    const eventName = 'examination_area_event_' + mainContext?.profession?.examination_area_id;
+    const refresh = () => {
+        setReload(!reload);
+    };
+    const openNotification = (description, message, result) => {
+        if (parseInt(result)) {
+            notification.success({
+                message: message,
+                description: description,
+            });
+        } else {
+            notification.error({
+                message: message,
+                description: description,
+            });
+        }
+    };
     useEffect(() => {
         (async () => {
             const resp = await getExamProcessComputers();
             setData(resp?.data)
-        })()
+            console.log('change dan oldin', resp?.data)
+        })();
+
     }, [reload]);
+    useEffect(() => {
+        if (parseInt(mainContext?.profession?.examination_area_id) && data.length) {
+            console.log('eventName', eventName);
+            socketParam.on(eventName, (dataSocket) => {
+                openNotification(dataSocket?.message, dataSocket?.userName, dataSocket?.test_result);
+                console.log('bundan oldin', data);
+                const newState = data.map((obj, index) => {
+                    if (parseInt(obj.id) === parseInt(dataSocket?.computer_id)) {
+                        return  {...obj, merge: null};
+                    }
+                    return obj;
+                });
+                setData(newState);
+                console.log('change dan keyin', newState);
+                console.log('keldi', dataSocket)
+            });
+            return () => {
+                socketParam.off(eventName);
+            }
+        }
+    }, [mainContext?.profession?.examination_area_id,data]);
     const clearComputer = (computer) => {
         (async () => {
             const res = await clearComputerApi(computer?.id);
@@ -22,15 +66,14 @@ const ExamProcessByComputerTab = (props) => {
                 setReload(!reload)
             }
         })()
-    }
+    };
     const computerEndTest = (final_access_student_id) => {
         (async () => {
             const res = await computerTestEndApi(final_access_student_id);
             if (parseInt(res?.status) === 1) {
                 message.success(res?.message);
                 setReload(!reload)
-            }
-            else{
+            } else {
                 message.error(res?.message);
 
             }
@@ -55,7 +98,8 @@ const ExamProcessByComputerTab = (props) => {
                                           {
                                               element?.merge ?
                                                   <Popconfirm title={'Kompyuterni bo`shatasizmi ?'}
-                                                              okText={'Bo`shatish'} cancelText={'Bekor qilish'} onConfirm={() => clearComputer(element)}>
+                                                              okText={'Bo`shatish'} cancelText={'Bekor qilish'}
+                                                              onConfirm={() => clearComputer(element)}>
                                                   <span style={{fontSize: '25px', color: '#52c41a'}}><i
                                                       className={'bx bxs-brush-alt'}/></span></Popconfirm> :
                                                   <span style={{fontSize: '25px', color: '#faad14'}}><i
@@ -68,7 +112,8 @@ const ExamProcessByComputerTab = (props) => {
                                           {
                                               element?.merge ?
                                                   <Popconfirm title={'Testni tugatasizmi ?'} okText={'Tugatish'}
-                                                              cancelText={'Bekor qilish'} onConfirm={()=>computerEndTest(element?.merge?.final_access_student?.id)}>
+                                                              cancelText={'Bekor qilish'}
+                                                              onConfirm={() => computerEndTest(element?.merge?.final_access_student?.id)}>
                                               <span style={{fontSize: '25px', color: '#f5222d'}}><i
                                                   class='bx bx-x'/></span>
                                                   </Popconfirm>
