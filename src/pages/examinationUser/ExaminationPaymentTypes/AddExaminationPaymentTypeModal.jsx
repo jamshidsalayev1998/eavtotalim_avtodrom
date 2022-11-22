a7d390a66e0c4525a4d06ebbb396b853f2cb47e9
webdeveloper2023 <nazirovrafiqjon95@gmail.com>
2022-11-22 09:36:21 +0500

    price modal select is added
    price modal select is added

--- src/pages/examinationUser/ExaminationPaymentTypes/AddExaminationPaymentTypeModal.jsx
+++ src/pages/examinationUser/ExaminationPaymentTypes/AddExaminationPaymentTypeModal.jsx
@@ -1,119 +1,148 @@
 import React from "react";
-import {Modal, Form, Input, Row, Col, message, Select} from "antd";
-import {storeExaminationPaymentType} from "../../../services/api_services/examination_payment_types";
+import { Modal, Form, Input, Row, Col, message, Select } from "antd";
+import { storeExaminationPaymentType } from "../../../services/api_services/examination_payment_types";
 
-const AddExaminationPaymentTypeModal = ({setIsAddModalVisible, isAddModalVisible, reload, setReload}) => {
-    const [addForm] = Form.useForm();
-    const handleOk = () => {
-        addForm.submit()
-    };
-    const {Option} = Select;
+const AddExaminationPaymentTypeModal = ({
+  setIsAddModalVisible,
+  isAddModalVisible,
+  reload,
+  setReload,
+}) => {
+  const [addForm] = Form.useForm();
+  const handleOk = () => {
+    addForm.submit();
+  };
+  const { Option } = Select;
 
-    const handleCancel = () => {
+  const handleCancel = () => {
+    setIsAddModalVisible(false);
+  };
+  const onFinishAdd = values => {
+    const formData = new FormData();
+    for (const key in values) {
+      formData.append(key, values[key] ? values[key] : "");
+    }
+    (async () => {
+      const response = await storeExaminationPaymentType(formData);
+      if (response?.data?.status == 1) {
+        setReload(!reload);
         setIsAddModalVisible(false);
-    };
-    const onFinishAdd = (values) => {
-        const formData = new FormData();
-        for (const key in values) {
-            formData.append(key, values[key] ? values[key] : '');
-        }
-        (
-            async () => {
-                const response = await storeExaminationPaymentType(formData);
-                if (response?.data?.status == 1) {
-                    setReload(!reload)
-                    setIsAddModalVisible(false)
-                    addForm.resetFields();
-                    message.success(response?.data?.message);
-                }
-            }
-        )()
+        addForm.resetFields();
+        message.success(response?.data?.message);
+      }
+    })();
+  };
+  const onFinishFailedAdd = () => {};
 
-    }
-    const onFinishFailedAdd = () => {
-
-    }
-
-    return (
-        <>
-            <Modal title="To`lov turi qo`shish"
-                   visible={isAddModalVisible}
-                   onOk={handleOk}
-                   onCancel={handleCancel}
-                   okText={'Saqlash'}
-                   cancelText={'Bekor qilish'}
-                   zIndex={1005}
+  return (
+    <>
+      <Modal
+        title="To`lov turi qo`shish"
+        visible={isAddModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText={"Saqlash"}
+        cancelText={"Bekor qilish"}
+        zIndex={1005}
+      >
+        <Row>
+          <Col xl={24}>
+            <Form
+              form={addForm}
+              name="addPaymentTypeForm"
+              labelCol={{
+                span: 24,
+              }}
+              wrapperCol={{
+                span: 24,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinishAdd}
+              onFinishFailed={onFinishFailedAdd}
+              autoComplete="off"
             >
-                <Row>
-                    <Col xl={24}>
-                        <Form
-                            form={addForm}
-                            name="addPaymentTypeForm"
-                            labelCol={{
-                                span: 24,
-                            }}
-                            wrapperCol={{
-                                span: 24,
-                            }}
-                            initialValues={{
-                                remember: true,
-                            }}
-                            onFinish={onFinishAdd}
-                            onFinishFailed={onFinishFailedAdd}
-                            autoComplete="off"
-                        >
-                            <Form.Item
-                                label="Nomi"
-                                name="name"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Nomini kiriting!',
-                                    },
-                                ]}
-                            >
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Summasi"
-                                name="amount"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Summasini kiriting!',
-                                    },
-                                ]}
-                            >
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item
-                                label="Turi"
-                                name="exam_type"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Summasini kiriting!',
-                                    },
-                                ]}
-                                initialValue={'practical'}
-                            >
-                                <Select
-                                    showSearch
-                                    placeholder="Select a person"
-                                    optionFilterProp="children"
-                                    filterOption={(input, option) =>
-                                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                                    }
-                                >
-                                    <Option value="practical">Amaliy</Option>
-                                    <Option value="theoretical">Nazariy</Option>
-                                </Select>
-                            </Form.Item>
-                        </Form>
-                    </Col>
-                </Row>
-            </Modal>
-        </>
-    )
-}
-export default (AddExaminationPaymentTypeModal);
+              <Form.Item
+                label="Nomi"
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: "Nomini kiriting!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Summasi"
+                name="amount"
+                rules={[
+                  {
+                    required: true,
+                    message: "Summasini kiriting!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Turi"
+                name="exam_type"
+                rules={[
+                  {
+                    required: true,
+                    message: "Summasini kiriting!",
+                  },
+                ]}
+                initialValue={"practical"}
+              >
+                <Select
+                  showSearch
+                  placeholder="Select a person"
+                  optionFilterProp="children"
+                  filterOption={(input, option) =>
+                    option.children
+                      .toLowerCase()
+                      .indexOf(input.toLowerCase()) >= 0
+                  }
+                >
+                  <Option value="practical">Amaliy</Option>
+                  <Option value="theoretical">Nazariy</Option>
+                </Select>
+              </Form.Item>
+
+              <Form.Item
+                label="To'lov maqsadi"
+                name="exam_reason"
+                rules={[
+                  {
+                    required: true,
+                    message: "To'lov maqsadini kiriting",
+                  },
+                ]}
+                initialValue={"first"}
+              >
+                <Select
+                  showSearch
+                  placeholder="To'lov maqsadi"
+                  optionFilterProp="children"
+                  filterOption={(input, option) =>
+                    option.children
+                      .toLowerCase()
+                      .indexOf(input.toLowerCase()) >= 0
+                  }
+                >
+                  <Option value="first">Birinchi marta</Option>
+                  <Option value="return">Qayta topshirish</Option>
+                </Select>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </Modal>
+    </>
+  );
+};
+export default AddExaminationPaymentTypeModal;
