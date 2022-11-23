f518e253c5305480f26a4e4f796be34dcd25ad79
jamshidsalayev1998 <jmlaravel@mail.ru>
2022-11-23 10:31:45 +0500

    asd

--- src/pages/examinationUser/AdministratorCrud/pages/AllStudents/AllStudentsIndex.jsx
+++ src/pages/examinationUser/AdministratorCrud/pages/AllStudents/AllStudentsIndex.jsx
@@ -9,7 +9,6 @@ import QRCode from "qrcode";
 import QrCodeToPrint from "./QrCodeToPrint";
 import {NavLink, useHistory, useLocation} from "react-router-dom";
 import "./key_styles.css"
-import {socketParam} from "../../../../../App";
 
 
 const AllStudentsIndex = props => {
@@ -32,9 +31,6 @@ const AllStudentsIndex = props => {
     useEffect(() => {
         const token = localStorage.getItem('token');
         setIsloading(true)
-         socketParam.on('practical_socket_ex_21_car_1' , (data) => {
-            alert('anaa muhammadjon ishladiku')
-        });
         axios({
             url: PATH_PREFIX + '/examination-administrator/all-students',
             method: 'GET',
