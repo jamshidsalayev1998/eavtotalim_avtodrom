ae9ef717528c74fb785b0fd5e0ace0e1da49d56c
jamshidsalayev1998 <jmlaravel@mail.ru>
2022-11-25 20:21:53 +0500

    asd

--- src/pages/examinationUser/Computers/Config/ComputerSettingsPage.jsx
+++ src/pages/examinationUser/Computers/Config/ComputerSettingsPage.jsx
@@ -1,12 +1,14 @@
 import React, {Fragment, useContext, useEffect, useState} from "react";
 import logoImg from "assets/images/logo-blue-big.png";
-import faceShape from "assets/images/face-recognition/face-shape.png";
+import faceShape from "assets/images/face-recognition/face-shape-warning.png";
+import faceShapeSuccess from "assets/images/face-recognition/face-shape-success.png";
+import faceShapeDanger from "assets/images/face-recognition/face-shape-danger.png";
 import {
     checkByComputerKey, faceRecognition,
     loginByComputerKey,
 } from "../../../../services/api_services/auth/login_computer_api";
 import {PATH_PREFIX_FILE} from "../../../../Utils/AppVariables";
-import {Button, Empty, message, Result} from "antd";
+import {Button, Empty, message, Result, notification} from "antd";
 import MainContext from "../../../../Context/MainContext";
 import {useHistory} from "react-router";
 import {Link, NavLink} from "react-router-dom";
@@ -17,6 +19,7 @@ import {
 } from "react-icons/ai";
 import {BsFillFileEarmarkMedicalFill} from "react-icons/bs";
 import Webcam from 'react-webcam'
+import "./style.css"
 
 const WebcamComponent = () => <Webcam/>
 const videoConstraints = {
@@ -33,25 +36,72 @@ const ComputerSettingsPage = () => {
     const history = useHistory();
     const [faceRecognitionKey, setFaceRecognitionKey] = useState(localStorage.getItem('face_recognition_key'))
     const [picture, setPicture] = useState('')
-    const [loadingRecognition , setLoadingRecognition] = useState(false);
+    const [loadingRecognition, setLoadingRecognition] = useState(false);
     const webcamRef = React.useRef(null)
     const faceRecognitionFunction = React.useCallback(() => {
+        setLoadingRecognition(true)
         const pictureSrc = webcamRef.current.getScreenshot()
         setPicture(pictureSrc)
-         if (computer && computerKey) {
+        if (computer && computerKey) {
             (async () => {
                 const data = new FormData();
                 data.append("computer_key", computerKey);
                 data.append("image", pictureSrc);
                 const resp = await faceRecognition(data);
-                if (parseInt(resp?.status) === 1) {
-                    if (resp?.face_recognition_key) {
-                        setFaceRecognitionKey(resp?.face_recognition_key)
-                        localStorage.setItem('face_recognition_key',resp?.face_recognition_key)
+                if (parseInt(resp?.result_code) === 1) {
+                    if (resp?.response_id) {
+                        setFaceRecognitionKey(resp?.response_id)
+                        localStorage.setItem('face_recognition_key', resp?.response_id)
+                        message.success(resp?.result_note);
+                        setLoadingRecognition(false);
+                        loginByComputer();
                     }
-                } else if (parseInt(resp?.status) === 0) {
-                    message.error(resp?.message);
+                } else if (parseInt(resp?.result_code) === 2) {
+                    // message.error("Pasrtpo malumotlari noto'g'ri kiritilgan");
                     setPicture('')
+                    notification.open({
+                        message: 'Xatolik',
+                        description:
+                            'Pasrtpo malumotlari noto\'g\'ri kiritilgan',
+                    });
+                } else if (parseInt(resp?.result_code) === 4) {
+                    // message.error('Pasport malumotlari bilan mos tushmadi');
+                    setPicture('')
+                    notification.open({
+                        message: 'Xatolik',
+                        description:
+                            'Pasport malumotlari bilan mos tushmadi',
+                    });
+                } else if (parseInt(resp?.result_code) === 17) {
+                    // message.error('Yuzingizni kameraga to`g`ri tuting');
+                    setPicture('')
+                    notification.open({
+                        message: 'Xatolik',
+                        description:
+                            'Yuzingizni kameraga to`g`ri tuting',
+                    });
+                } else if (parseInt(resp?.result_code) === 21) {
+                    // message.error("Yuzingiz kameraga to'liq ko'rinsin");
+                    setPicture('')
+                    notification.open({
+                        message: 'Xatolik',
+                        description:
+                            'Yuzingiz kameraga to`iq ko`rinsin',
+                    });
+                } else if (parseInt(resp?.status) === 0) {
+                    // message.error(resp?.message);
+                    setPicture('')
+                    notification.open({
+                        message: 'Xatolik',
+                        description: resp?.message,
+                    });
+                } else {
+                    // message.error("Mos emas");
+                    setPicture('')
+                    notification.open({
+                        message: 'Xatolik',
+                        description: 'Mos emas',
+                    });
                 }
             })();
         }
@@ -95,6 +145,8 @@ const ComputerSettingsPage = () => {
                     message.error(resp?.message);
                 } else if (parseInt(resp?.status) === 3 || parseInt(resp?.status) === 2) {
                     message.error('Yuz orqali identifikatsiya qiling')
+                    setFaceRecognitionKey('');
+                    localStorage.removeItem('face_recognition_key')
                 }
             })();
         }
@@ -113,11 +165,9 @@ const ComputerSettingsPage = () => {
     const handleKeyBtn = event => {
         event.preventDefault();
         if (event?.keyCode === 13) {
-            if (faceRecognitionKey){
+            if (faceRecognitionKey) {
                 loginByComputer();
-
-            }
-            else{
+            } else {
                 takePictureAndRecognition();
             }
         }
@@ -201,9 +251,17 @@ const ComputerSettingsPage = () => {
                         <div className={"text-center"}>
                             {/*take picture*/}
                             <div>
-                                <div className={'p-2'}>
-                                    <div className={'face-shape-box'} style={{position:'absolute',top:'50%' , left:'50%',transform:'translate(-50%,-50%)',width:'200px',height:'250px'}}>
-                                        <img src={faceShape} alt="" style={{width:'100%' , height:'100%'}}/>
+                                <div className={'p-2 '} style={{position: "relative"}}>
+                                    <div className={picture ? 'scanning' : ''}></div>
+                                    <div className={'face-shape-box  '} style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        transform: 'translate(-50%,-50%)',
+                                        width: '200px',
+                                        height: '250px'
+                                    }}>
+                                        <img src={faceShape} alt="" style={{width: '100%', height: '100%'}}/>
                                     </div>
                                     {picture == '' ? (
                                         <Webcam
@@ -216,6 +274,7 @@ const ComputerSettingsPage = () => {
                                             videoConstraints={videoConstraints}
                                         />
                                     ) : (
+
                                         <img style={{borderRadius: '20px'}} src={picture}/>
                                     )}
                                 </div>
@@ -232,7 +291,7 @@ const ComputerSettingsPage = () => {
                                     autoFocus
                                 >
                                     <span style={{fontSize: "35px"}}>{
-                                        faceRecognitionKey ? 'Testni boshlash' :'Boshlash'
+                                        faceRecognitionKey ? 'Testni boshlash' : 'Boshlash'
                                     }</span>{" "}
                                     <span
                                         className={"functional_key ml-3 "}
