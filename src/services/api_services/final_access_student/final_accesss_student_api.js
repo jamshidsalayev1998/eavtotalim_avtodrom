5b101c8474bfc830a3542ee8fdf74142d9ad8ce1
webdeveloper2023 <nazirovrafiqjon95@gmail.com>
2023-05-31 14:49:28 +0500

    asdsad
    adsadsa

--- src/services/api_services/final_access_student/final_accesss_student_api.js
+++ src/services/api_services/final_access_student/final_accesss_student_api.js
@@ -1,44 +1,64 @@
+import API_V2 from "api/index_v2";
 import API from "../../../api";
 
-
-export const updateFinalAccessStudent = async (itemId,data,params={}) => {
-    try {
-        const token = localStorage.getItem("token");
-        Object.assign(params, {token: token});
-        const response = await API({
-            url: "/examination-area/final-access-student-edit/"+itemId,
-            method: "POST",
-            params: params,
-            headers: {
-                'Accept': 'application/json'
-            },
-            data:data
-        });
-        if (response?.data) {
-            return response?.data;
-        }
-    } catch (error) {
-        return error
+export const updateFinalAccessStudent = async (itemId, data, params = {}) => {
+  try {
+    const token = localStorage.getItem("token");
+    Object.assign(params, { token: token });
+    const response = await API({
+      url: "/examination-area/final-access-student-edit/" + itemId,
+      method: "POST",
+      params: params,
+      headers: {
+        Accept: "application/json",
+      },
+      data: data,
+    });
+    if (response?.data) {
+      return response?.data;
     }
+  } catch (error) {
+    return error;
+  }
+};
+// old update student
+export const editFinalAccessStudent = async (itemId, params = {}) => {
+  try {
+    const token = localStorage.getItem("token");
+    Object.assign(params, { token: token });
+    const response = await API({
+      url: "/examination-area/final-access-student-edit/" + itemId,
+      method: "GET",
+      params: params,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    if (response?.data) {
+      return response?.data;
+    }
+  } catch (error) {
+    return error;
+  }
 };
 
-export const editFinalAccessStudent = async (itemId,params={}) => {
-    try {
-        const token = localStorage.getItem("token");
-        Object.assign(params, {token: token});
-        const response = await API({
-            url: "/examination-area/final-access-student-edit/"+itemId,
-            method: "GET",
-            params: params,
-            headers: {
-                'Accept': 'application/json'
-            },
-        });
-        if (response?.data) {
-            return response?.data;
-        }
-    } catch (error) {
-        return error
+// new update student
+export const showFinalAccessStudent = async (itemId, params = {}) => {
+  try {
+    const token = localStorage.getItem("token");
+    Object.assign(params, { token: token });
+    const response = await API_V2({
+      url: "/examination-administrator/students/" + itemId,
+      method: "GET",
+      params: params,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    if (response?.data) {
+      return response?.data;
     }
+  } catch (error) {
+    return error;
+  }
 };
-
