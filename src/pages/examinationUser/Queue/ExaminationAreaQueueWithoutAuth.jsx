5c455055cea56263230fed5b9d4ed7bfca248d04
jamshidsalayev1998 <jmlaravel@mail.ru>
2023-06-03 20:01:20 +0500

    asd

--- src/pages/examinationUser/Queue/ExaminationAreaQueueWithoutAuth.jsx
+++ src/pages/examinationUser/Queue/ExaminationAreaQueueWithoutAuth.jsx
@@ -3,14 +3,26 @@ import {get_queue_info} from "../../../services/api_services/queue/queu_without_
 import {Col, Row} from "antd";
 import ListQueueComponent from "./ListQueueComponent";
 import {useHistory, useRouteMatch} from "react-router";
+import {socketParam} from "../../../App";
+import "./style.css"
 
 const ExaminationAreaQueueWithoutAuth = () => {
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
     const [remainingQueueData, setRemainingQueueData] = useState([]);
     const [limitedQueueData, setLimitedQueueData] = useState([]);
+    const [freeComputersData, setFreeComputersData] = useState([]);
     const match = useRouteMatch('/queue/:id');
     useEffect(() => {
-getInfo();
+        getInfo();
+        const eventName = 'queue_event_' + match?.params?.id;
+        socketParam.on(eventName, data => {
+            reBuildQueue(data)
+            console.log('keyin remaining', remainingQueueData);
+            console.log('keyin limit', limitedQueueData)
+        });
+        return () => {
+            socketParam.off(eventName);
+        };
     }, []);
     const getInfo = () => {
         (async () => {
@@ -19,17 +31,46 @@ getInfo();
             setData(response);
             setRemainingQueueData(response?.remainingQueueData);
             setLimitedQueueData(response?.limitedQueueData);
+            setFreeComputersData(response?.freeComputersData);
         })()
+    };
+    const transferElement = (data) => {
+        console.log('socket', data?.type);
+        if (remainingQueueData.length > 0) {
+            const firstElement = remainingQueueData[0];
+            setRemainingQueueData(prevData => prevData.slice(1));
+            setLimitedQueueData(prevData => [...prevData, firstElement]);
+        }
+    };
+    const reBuildQueue = (data) => {
+        if (data?.type === 'new_student') {
+            if (parseInt(freeComputersData) > limitedQueueData.length) {
+                setLimitedQueueData(prevData => [...prevData, data?.queue]);
+            } else {
+                setRemainingQueueData(prevData => [...prevData, data?.queue]);
+            }
+        } else if (data?.type === 'free_computer') {
+            transferElement(data);
+        }
+        else if (data?.type === 'student_merged'){
+            removeElement(data?.unikal_number)
+        }
+    };
+    const removeElement = (unikal_number) => {
+        const updatedQueueData = limitedQueueData.filter(
+            element => element.unikal_number !== unikal_number
+        );
+        setLimitedQueueData(updatedQueueData);
     };
     return (
         <Row>
-            <Col xl={12} className={{backgroundColor:'red',padding:'10px'}}>
+            <Col xl={12} className={'.queueRemainingBoxClass'}>
                 <h5>Kutishda</h5>
-                <ListQueueComponent data={remainingQueueData} />
+                <ListQueueComponent data={remainingQueueData}/>
             </Col>
-            <Col xl={12} className={{backgroundColor:'red',padding:'10px'}}>
+            <Col xl={12} className={'.queueLimitBoxClass'}>
                 <h5>Kirishi mumkin</h5>
-                <ListQueueComponent data={limitedQueueData} />
+                <ListQueueComponent data={limitedQueueData}/>
             </Col>
         </Row>
     )
--- src/pages/examinationUser/Queue/ListQueueComponent.jsx
+++ src/pages/examinationUser/Queue/ListQueueComponent.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {Col, Row} from "antd";
+import "./style.css"
 
 const ListQueueComponent = ({data}) =>{
     return (
         <Row>
             {data?.length ? data?.map((element , index) => {
                 return (
-                    <Col xl={3} key={index}>{element?.unikal_number}</Col>
+                    <Col className={'queueElementClass'} xl={3} key={index}>{element?.unikal_number}</Col>
                 )
             }):null}
         </Row>
