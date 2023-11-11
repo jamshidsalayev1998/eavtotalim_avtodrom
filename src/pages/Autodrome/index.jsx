91b10f0c91a5fe5faa586fcb077fc4246cd39fdd
abdugafur <abdugafur3323@gmail.com>
2023-11-11 13:26:53 +0500

    some changes

--- src/pages/Autodrome/index.jsx
+++ src/pages/Autodrome/index.jsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {Link, NavLink} from "react-router-dom";
-import {Table, Tbody, Th, Thead, Tr} from "react-super-responsive-table";
-import {PATH_PREFIX_FILE, PATH_PREFIX_V2} from "../../Utils/AppVariables";
-import {useTranslation} from "react-i18next";
-import {Badge, Card, CardBody} from "reactstrap";
-import {DataLoader} from "../Loaders/Loaders";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Table, Tbody, Th, Thead, Tr } from "react-super-responsive-table";
+import { PATH_PREFIX_FILE, PATH_PREFIX_V2 } from "../../Utils/AppVariables";
+import { useTranslation } from "react-i18next";
+import { Badge, Card, CardBody } from "reactstrap";
+import { DataLoader } from "../Loaders/Loaders";
 import PaginationComponent from "react-reactstrap-pagination";
 import axios from "axios";
-import {Tooltip} from "antd";
+import { Tooltip } from "antd";
 
 const Autodrome = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
 
   const token = localStorage.getItem("token");
 
@@ -24,42 +24,51 @@ const Autodrome = () => {
   });
 
   const getAutodromes = async () => {
-    return await axios.get(`${PATH_PREFIX_V2}/examination-area-organization-agreement`, {
-      params: {
-        token: token,
-        page: selected_count
-      }
-    }).then(res => {
-      console.log('res', res.data)
-      setAutodromes({
-        loaded: true,
-        total: res.data.data?.length,
-        data: res.data?.data,
-        error: null,
+    return await axios
+      .get(`${PATH_PREFIX_V2}/examination-area-organization-agreement`, {
+        params: {
+          token: token,
+          page: selected_count,
+        },
+      })
+      .then(res => {
+        console.log("res", res.data);
+        setAutodromes({
+          loaded: true,
+          total: res.data.data?.length,
+          data: res.data?.data,
+          error: null,
+        });
+      })
+      .catch(err => {
+        console.log("error", err);
       });
-    }).catch(err => {
-      console.log('error', err)
-    });
-  }
+  };
 
   useEffect(() => {
-    getAutodromes()
-  }, [selected_count])
+    getAutodromes();
+  }, [selected_count]);
 
   const agreementHandle = async (id, key, data) => {
-    let res = await axios.post(`${PATH_PREFIX_V2}/examination-area-organization-agreement/${key === "accept" ? "accept" : "reject"}/${id}`, data, {
-      headers: {
-        'AUTHORIZATION': `Bearer ${token}`
+    let res = await axios.post(
+      `${PATH_PREFIX_V2}/examination-area-organization-agreement/${
+        key === "accept" ? "accept" : "reject"
+      }/${id}`,
+      data,
+      {
+        headers: {
+          AUTHORIZATION: `Bearer ${token}`,
+        },
       }
-    })
+    );
     if (res.status === 200) {
-      await getAutodromes()
+      await getAutodromes();
     }
-  }
+  };
 
-  const handleSelected = (selectedPage) => {
+  const handleSelected = selectedPage => {
     setSelected_count(selectedPage);
-  }
+  };
 
   return (
     <>
@@ -76,9 +85,13 @@ const Autodrome = () => {
             <div className="d-flex justify-content-between align-items-center">
               <h5 className="text-dark">{t("autodrome")}</h5>
             </div>
-            {
-              !autodromes.loaded ? <DataLoader/> : <Table id="autodrome"
-                                                          className="table table-striped table-bordered">
+            {!autodromes.loaded ? (
+              <DataLoader />
+            ) : (
+              <Table
+                id="autodrome"
+                className="table table-striped table-bordered"
+              >
                 <Thead className="font-size-14">
                   <Tr>
                     <Th data-priority="3">№</Th>
@@ -91,60 +104,99 @@ const Autodrome = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {
-                    autodromes?.data?.map((el, index) => {
-                      return (
-                        <Tr key={el?.id}>
-                          <Th className="">{index + 1}</Th>
-                          <Th className="table_body">
-                            {el?.organization?.name}
-                          </Th>
-                          <Th className="table_body">
-                            {el?.agreement_number}
-                          </Th>
-                          <Th className="table_body">{el?.agreement_expiration_date}</Th>
-                          <Th className="table_body">
-                            <Badge color={el.status === 0 ? "primary" : el.status === 2 ? "warning" : "success"}
-                                   className="py-1 px-2 badge badge-pill">
-                              {el.status === 0 ? `${t("new")}` : el.status === 1 ? `${t("confirmed")}` : el.status === 2 ? `${t("rejected")}` : `${t("confirmed_archive")}`}</Badge>
-                          </Th>
-                          <Th>
-                            <a className="link_to_print" href={PATH_PREFIX_FILE + el?.agreement} download
-                               target="_blank" rel="noreferrer">
-                              <i className="bx bx-printer"/>
-                            </a>
-                          </Th>
-                          <Th className="table_body " style={{width: "80px"}}>
-                            {
-                              el?.status === 0 ? <div className="func_buttons">
-                                <Tooltip title={t('confirm')}>
-                                  <span onClick={() => agreementHandle(el?.id, "accept", el)}><i
-                                    className="bx bx-check font-size-24"></i></span>
-                                </Tooltip>
-                                <Tooltip title={t('reject')}>
-                                  <span onClick={() => agreementHandle(el?.id, 'reject', el)}><i
-                                    className="bx bx-x font-size-18 mr-3 text-danger"></i></span>
-                                </Tooltip>
-                              </div> :  el?.status === 1 ? <Tooltip title={t('confirmed')}>
-                                <span><i className="bx bx-check font-size-24"></i></span>
-                              </Tooltip> : el?.status === 2 ? <Tooltip title={t('rejected')}>
-                                <span><i className="bx bx-x font-size-24"></i></span>
-                              </Tooltip> :  el?.status === 2 ? <Tooltip title={t('confirmed_archive')}>
-                                <span><i className="bx bx-archive font-size-24"></i></span>
-                              </Tooltip> : ""
+                  {autodromes?.data?.map((el, index) => {
+                    return (
+                      <Tr key={el?.id}>
+                        <Th className="">{index + 1}</Th>
+                        <Th className="table_body">{el?.organization?.name}</Th>
+                        <Th className="table_body">{el?.agreement_number}</Th>
+                        <Th className="table_body">
+                          {el?.agreement_expiration_date}
+                        </Th>
+                        <Th className="table_body">
+                          <Badge
+                            color={
+                              el.status === 0
+                                ? "primary"
+                                : el.status === 2
+                                ? "warning"
+                                : "success"
                             }
-                          </Th>
-                        </Tr>
-                      )
-                    })
-                  }
+                            className="py-1 px-2 badge badge-pill"
+                          >
+                            {el.status === 0
+                              ? `${t("new")}`
+                              : el.status === 1
+                              ? `${t("confirmed")}`
+                              : el.status === 2
+                              ? `${t("rejected")}`
+                              : `${t("confirmed_archive")}`}
+                          </Badge>
+                        </Th>
+                        <Th>
+                          <a
+                            className="link_to_print"
+                            href={PATH_PREFIX_FILE + el?.agreement}
+                            download
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <i className="bx bx-printer" />
+                          </a>
+                        </Th>
+                        <Th className="table_body " style={{ width: "80px" }}>
+                          {el?.status === 0 ? (
+                            <div className="func_buttons">
+                              <Tooltip title={t("confirm")}>
+                                <span
+                                  onClick={() =>
+                                    agreementHandle(el?.id, "accept", el)
+                                  }
+                                >
+                                  <i className="bx bx-check font-size-24"></i>
+                                </span>
+                              </Tooltip>
+                              <Tooltip title={t("reject")}>
+                                <span
+                                  onClick={() =>
+                                    agreementHandle(el?.id, "reject", el)
+                                  }
+                                >
+                                  <i className="bx bx-x font-size-18 mr-3 text-danger"></i>
+                                </span>
+                              </Tooltip>
+                            </div>
+                          ) : el?.status === 1 ? (
+                            <Tooltip title={t("confirmed")}>
+                              <span>
+                                <i className="bx bx-check font-size-24"></i>
+                              </span>
+                            </Tooltip>
+                          ) : el?.status === 2 ? (
+                            <Tooltip title={t("rejected")}>
+                              <span>
+                                <i className="bx bx-x font-size-24"></i>
+                              </span>
+                            </Tooltip>
+                          ) : el?.status === 2 ? (
+                            <Tooltip title={t("confirmed_archive")}>
+                              <span>
+                                <i className="bx bx-archive font-size-24"></i>
+                              </span>
+                            </Tooltip>
+                          ) : (
+                            ""
+                          )}
+                        </Th>
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
-            }
+            )}
             {autodromes.data?.length > 0 && (
               <div className="table_bottom_tools_custom">
-                <div className="show_responsive">
-                </div>
+                <div className="show_responsive"></div>
                 <div className="px-0 pagination_responsive">
                   <PaginationComponent
                     totalItems={autodromes.total}
@@ -153,14 +205,13 @@ const Autodrome = () => {
                     defaultActivePage={selected_count}
                   />
                 </div>
-
               </div>
             )}
           </CardBody>
         </Card>
       </div>
     </>
-  )
+  );
 };
 
 export default Autodrome;
